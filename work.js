106ee6a55228523d98a8594345f985772cd9c83d
htf11 <alex.inck2000@yandex.ru>
2025-03-17 12:17:08 +0300

    add .gitignore file

--- work.js
+++
@@ -1,6 +0,0 @@
-const http = require('http'); //Подключаем модуль http
-const port = 3000;
-const server = http.createServer((req, res) => { //Создаем сервер
-  res.end('Hello world!'); //Если кто то подключился отвечаем.
-}).listen(3000); //Слушаем порт 3000
-console.log('Сервер работает на порте ' + port); //Выводим сообщение в консоль
